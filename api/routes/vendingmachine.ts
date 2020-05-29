957ef3c4d951b674e03173978f4f538df405eca9
German Dario <germangp088@gmail.com>
2020-05-29 15:37:42 -0300

    Add validation

--- api/routes/vendingmachine.ts
+++ api/routes/vendingmachine.ts
@@ -18,8 +18,13 @@ router.get('/cash', (_req: express.Request, res: express.Response) => {
 });
 
 router.post('/cash', (req: express.Request, res: express.Response) => {
-  vendingMachine.cash = parseFloat(eval(vendingMachine.cash.toString()) + eval(req.body.price)).toFixed(2);
-  res.status(200).json(vendingMachine.cash);
+  console.log(req.body)
+  if(!req.body.price || isNaN(req.body.price)) {
+    res.status(400).end();
+  } else {
+    vendingMachine.cash = parseFloat(eval(vendingMachine.cash.toString()) + eval(req.body.price)).toFixed(2);
+    res.status(200).json(vendingMachine.cash);
+  }
 });
 
 export default router
